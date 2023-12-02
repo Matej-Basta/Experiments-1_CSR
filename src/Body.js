1dfb8adc5aff54c583b60e0691a668fc93a27ef8
matej <matej.basta@email.cz>
2023-12-02 10:40:56 +0100

    initial structure

---
+++ src/Body.js
@@ -0,0 +1,15 @@
+import Article from './Article';
+
+const Body = ({numberOfElements}) => {
+    const articles = Array.from({length: numberOfElements}, (v, i) => (
+        <Article key={i} />));
+
+    return (
+        <>
+            <h1>Articles</h1>
+            <main>{articles}</main>
+        </>
+    );
+}
+
+export default Body;
